387f322767674e6d957874efab75ec9fb545edda
Stefanos Manavis <cs05276@uoi.gr>
2025-09-16 04:11:52 +0300

    finished continuous movement

--- core/src/main/java/com/snips/bh/actor/Player.java
+++ core/src/main/java/com/snips/bh/actor/Player.java
@@ -20,10 +20,13 @@ public class Player {
 
     public void update(float dt){
         float dx = 0, dy = 0;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.W)) dy += 1;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.S)) dy -= 1;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.A)) dx -= 1;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.D)) dx += 1;
+
+        // Hold-to-move: poll keys every frame
+        if (Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP))    dy += 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN))  dy -= 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT))  dx -= 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) dx += 1;
+
 
         //normalize diagonal
         if(dx != 0 || dy != 0){
