6524949a5e8771d525d100358025569b06270c11
Stefanos Manavis <cs05276@uoi.gr>
2025-09-18 20:40:11 +0300

    added background, player sprite, TODO: rotate around head and not leg, bullets smaller, blacker... enemy sprites

--- core/src/main/java/com/snips/bh/actor/Weapon.java
+++ core/src/main/java/com/snips/bh/actor/Weapon.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
+@SuppressWarnings("ALL")
 public class Weapon {
     // --- Config ---
     private float shotsPerSecond = 6f;
